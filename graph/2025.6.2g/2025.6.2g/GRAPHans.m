e69c8ae7df2815c0c369ac7186cec056051990e6
anushika32 <anushikawijerathna00@gmail.com>
2025-06-04 21:09:36 +0530

    update

---
+++ graph/2025.6.2g/2025.6.2g/GRAPHans.m
@@ -0,0 +1,5 @@
+s = [1 2] %start nodes
+t = [2 3] %end nodes
+G = graph(s, t); %create a graph object
+plog(G);
+title('Graph'); %ADD A TITLE
---
+++ graph/2025.6.2g/2025.6.2g/graphNew.m
@@ -0,0 +1,5 @@
+G = graph([1 2], [2 3]); %edges: 1-2 and
+plot(G);
+title('Undirected Graph');
+
+GH = digraph
